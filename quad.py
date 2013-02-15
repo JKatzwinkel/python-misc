32ce04d74bf84a4881cac0a24259aa776b15261b
thor <thor@mintj>
2013-02-15 22:37:08 +0100

    file tree disk usage visualizer: filter *.txt files and limit html element nesting

--- quad.py
+++ quad.py
@@ -3,6 +3,7 @@ import os
 
 _names=[]
 _diskusage={}
+_depth=40
 
 # http://wiki.python.org/moin/HowTo/Sorting/ 
 # http://stackoverflow.com/questions/955941/how-to-identify-whether-a-file-is-normal-file-or-directory-using-python
@@ -39,10 +40,10 @@ def resources(dirname):
 			filesize = disk_usage(os.path.join(dirname, sd))
 			du += filesize
 		 #disk usage of contained files
-		for fn in files:
+		for fn in filter(lambda x:x.endswith('.txt'), files):
 			filesize = disk_usage(os.path.join(dirname, fn))
 			du += filesize
-			results.append((dirname, fn, filesize))
+			results.append((dirname, fn.split('.txt')[0], filesize))
 		results.append((dirname, '', du+1))
 		# save directory disk use in dictionary
 		_diskusage[dirname] = du+1
@@ -71,42 +72,52 @@ def indent(level):
 	return '  '*level
 	
 
-def table_horizontal(items, full, level):
+def table_horizontal(items, full, level, attributes=''):
 	if len(items) < 1:
 		return
-	print indent(level)+'<table width="100%" height="100%">'
+	if len(items) < 2:
+		if items[0][0] != '':
+			print indent(level)+items[0][1]
+			return
+	print indent(level)+'<table width="100%" height="100%" {0}>'.format(attributes)
 	print indent(level+1)+'<tr>'
 	size = items[0][2]
 	print indent(level+2)+'<td width="{}%">'.format(size * 100 // full)
 	if items[0][1] == '':
-		if level < 15:
+		if level < _depth:
 			sub_items = largest(items[0][0])
-			table_vertical(sub_items[1:], sub_items[0][2], level+3)
+			table_vertical(sub_items[1:], sub_items[0][2], level+3, attributes='class="namespace"')
 		else:
 			print indent(level+3)+items[0][0]
 	else:
 		print indent(level+3)+items[0][1]
 	print indent(level+2)+'</td>'
 	print indent(level+2)+'<td>'
-	if level < 15:
+	if level < _depth:
 		table_vertical(items[1:], full - size, level+3)
 	else:
-		print indent(level+3)+items[1][1]
+		pass
+		#print indent(level+3)+items[1][1]
 	print indent(level+2)+'</td>'
 	print indent(level+1)+'</tr>'
 	print indent(level)+'</table>'
 	
-def table_vertical(items, full, level):
+	
+def table_vertical(items, full, level, attributes=''):
 	if len(items) < 1:
 		return
-	print indent(level)+'<table width="100%" height="100%">'
+	if len(items) < 2:
+		if items[0][0] != '':
+			print indent(level)+items[0][1]
+			return
+	print indent(level)+'<table width="100%" height="100%" {0}>'.format(attributes)
 	size = items[0][2]
 	print indent(level+1)+'<tr height="{}%">'.format(size * 100 // full)
 	print indent(level+2)+'<td>'
 	if items[0][1] == '':
-		if level < 15:
+		if level < _depth:
 			sub_items = largest(items[0][0])
-			table_horizontal(sub_items[1:], sub_items[0][2], level+3)
+			table_horizontal(sub_items[1:], sub_items[0][2], level+3, attributes='class="namespace"')
 		else:
 			print indent(level+3)+items[0][0]
 	else:
@@ -115,10 +126,11 @@ def table_vertical(items, full, level):
 	print indent(level+1)+'</tr>'
 	print indent(level+1)+'<tr>'
 	print indent(level+2)+'<td>'
-	if level < 15:
+	if level < _depth:
 		table_horizontal(items[1:], full - size, level+3)
 	else:
-		print indent(level+3)+items[1][1]
+		pass
+		#print indent(level+3)+items[1][1]
 	print indent(level+2)+'</td>'
 	print indent(level+1)+'</tr>'
 	print indent(level)+'</table>'
@@ -135,6 +147,8 @@ _names = resources('.')
 #for t in _names:
 #	print t
 
+			#border-collapse:collapse;
+
 print '''<!doctype html>
 <head>
 	<style type="text/css">
@@ -142,7 +156,8 @@ print '''<!doctype html>
 			border: 1px solid;
 			background-color: #DCF;
 			text-align:center;
-			font-family: 'sans-serif';
+			font-family: 'Arial', 'sans serif';
+			color: #A33;
 		}
 		td:hover {
 			background-color: #EDF;
@@ -153,8 +168,13 @@ print '''<!doctype html>
 			margin: 0px;
 		}
 		table {
+			width: 100%;
 			padding: 0px;
 			background-color: #DFD;
+			margin:0px;
+		}
+		table.namespace {
+			background-color: #BBF;
 		}
 		table:hover {
 			background-color: #ADA;
