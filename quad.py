bb1983748c0f5c4d4f12dee2887c8f1ae93be386
thor <thor@mintj>
2013-02-17 21:59:23 +0100

    files visualizer: fixed relative td/tr space coverage ratios, adjused css, introduced tooltips

--- quad.py
+++ quad.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 from random import randrange as rnd
+from math import log10 as log
 
 _names=[]
 _diskusage={}
@@ -136,43 +137,62 @@ def table_vertical(items, full, level, attributes=''):
 	print indent(level+1)+'</tr>'
 	print indent(level)+'</table>'
 	
+	
+def label((path, filename, diskuse), level):
+	if filename == '':
+		tableh(path, level+2)
+	else:
+		ns = ':'.join(path.split(os.sep)[1:]+[''])
+		link = '<a href="{0}">{1}</a>'
+		baseurl = 'https://wiki.linie/doku.php?id='
+		href = baseurl + ns + filename
+		label = filename
+		element = '<font size="{0}">{1}</font>'.format(1+log(diskuse)-1, link.format(href, label))
+		print '<span>{0}</span>'.format(filename)
+		print indent(level+2)+element
+
+# http://stackoverflow.com/questions/9725836/css-keep-table-cell-from-expanding-and-truncate-long-text
+# http://stackoverflow.com/questions/2736021/super-simple-css-tooltip-in-a-table-why-is-it-not-displaying-and-can-i-make-it
 def tableh(dirname, level):
 	items = largest(dirname)
-	directory = items.pop(0)
-	print indent(level)+'<table height="100%" class="namespace" dir={0}>'.format(['RTL', 'LTR'][rnd(2)])
+	path, filename, size = items.pop(0)
+	print indent(level)+'<table height="100%" class="tooltip" dir="{0}">'.format(['RTL', 'LTR'][level%2])
+	print indent(level+1)+'<span>{0}</span>'.format(dirname.split(os.sep)[-1])
 	#print indent(level+1)+'<caption>{0}</caption>'.format(directory[0].split(os.sep)[-1])
-	size = directory[2]
 	full = size
 	# loop through items / tr/td
 	# each tr contains two td
 	level += 1
+	remainder_h = 100.
+	remainder_v = 100.
+	
 	while len(items)>0:
 		rowspan = int(round(len(items)/2.))
-		item = items.pop(0)
-		size = item[2]
+		path, filename, size = items.pop(0)
 		print indent(level)+'<tr>'
+		
 		# first td
-		width = size * 100 // full
-		print indent(level+1)+'<td rowspan="{0}" width="{1}%">'.format(rowspan, width)
-		if item[1] == '':
-			tableh(item[0], level+2)
-		else:
-			print indent(level+2)+item[1]
+		covering = size * remainder_h / full
+		remainder_h -= covering
+		full -= size
+		print indent(level+1)+'<td class="tooltip" rowspan="{0}" width="{1}%" height="{2}%">'.format(rowspan, covering, remainder_v)
+		label((path, filename, size), level)
 		print indent(level+1)+'</td>'
+		
 		# second td
 		if len(items) > 0:
 			colspan = int(round(len(items)/2.))
-			item = items.pop(0)
-			size = item[2]
-			height = size * 100 // full
-			print indent(level+1)+'<td colspan="{0}" height="{1}%">'.format(colspan, height)
-			if item[1] == '':
-				tableh(item[0], level+2)
-			else:
-				print indent(level+2)+item[1]
+			path, filename, size = items.pop(0)
+			covering = size * remainder_v / full
+			remainder_v -= covering
+			full -= size
+			print indent(level+1)+'<td class="tooltip" colspan="{0}" width="{2}%" height="{1}%">'.format(colspan, covering, remainder_h)
+			label((path, filename, size), level)
 			print indent(level+1)+'</td>'
+			
 		print indent(level)+'</tr>'
 	print indent(level)+'</table>'
+
 
 def tagg(items):
 	full = items[0][2]
@@ -193,33 +213,48 @@ print '''<!doctype html>
 	<style type="text/css">
 		td {
 			border: 1px solid;
-			background-color: #DCF;
+			background-color: white;
 			text-align:center;
 			font-family: 'Arial', 'sans serif';
 			color: #A33;
+			display: table-cell; 
+			white-space: nowrap;
+			word-wrap: break-word;
+			text-overflow: ellipsis;
+			overflow:hidden;
 		}
 		td:hover {
-			background-color: white;
-		}
-		tr {
-			background-color: #FCD;
-			padding: 0px;
-			margin: 0px;
+			background-color: #DCF;
 		}
 		table {
 			width: 100%;
+			background-color: white;
 			padding: 0px;
-			background-color: #DFD;
 			margin:0px;
+			table-layout: fixed;
+			cellspacing: 5px;
 		}
 		table:hover {
-			background-color: #AFA;
+			background-color: #F0E0FF;
 		}
 		table.namespace {
 			background-color: #99D;
 		}
 		table.namespace:hover {
 			background-color: #C0C0FF;
+		}
+		.tooltip > span {
+			display: none;
+		}
+		.tooltip:hover > span {
+			display: block;
+			position: absolute;
+			background-color: #FFF;
+			border: 1px solid #CCC;
+			margin: 2px 10px;
+		}
+		table:hover[class~=tooltip] > span {
+			background-color: #FDD;
 		}
 	</style>
 </head>
