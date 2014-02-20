a55cd6c3dd102786eeafdd0812aec7a43bdf7661
J.Katzwinkel <jkatzwinkel@github.com>
2014-02-20 11:14:17 +0100

    better word recognition, bigger dictionary, read input text from text file

--- dictionary.py
+++ dictionary.py
@@ -11,7 +11,7 @@ def show(lvl, pref=''):
 			print('{}: {}x'.format(pref,l))
 
 
-wdex = re.compile('^[A-Za-z][a-z]{2,10}$') #, re.I)
+wdex = re.compile('^[A-Za-z][a-z]{2,16}$') #, re.I)
 voc = {}
 dep = 0
 for word in open('/usr/share/dict/american-english', 'r'):
@@ -30,21 +30,23 @@ for word in open('/usr/share/dict/american-english', 'r'):
 
 print('dict top level keys: {}; depth: {}.'.format(len(voc), dep))
 
-s = "hello&^uevfehello!`.<hellohow*howdhAreyou"
+#s = "hello&^uevfehello!`.<hellohow*howdhAreyou..."
+s=' '.join([line for line in open('text.txt', 'r')])
 
 found = []
-for i in range(len(s)):
+for i in range(len(s)-2):
 	lvl = voc
 	rec = ''
-	for c in s[i:i+10].lower():
+	for c in s[i:i+15].lower():
 		if '\n' in lvl:
 			found.append(rec)
 			lvl['\n'] = lvl.get('\n', 0) + 1
 		if c in lvl:
 			rec += c
-			lvl = lvl.get(c)
 		else:
 			break
+		lvl = lvl.get(c)
+
 print(found)
 
 show(voc)
