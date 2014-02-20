3f02bb5a206f984b3d9875004fe97f470da46eb6
Jakob Höper <jakobh@dariahst.(none)>
2014-02-20 19:21:49 +0100

    word recognition: better recog, better output

--- dictionary.py
+++ dictionary.py
@@ -11,7 +11,20 @@ def show(lvl, pref=''):
 			print('{}: {}x'.format(pref,l))
 
 
-wdex = re.compile('^[A-Za-z][a-z]{2,16}$') #, re.I)
+def ranks(lvl, pref=''):
+	items = []
+	for c,l in lvl.items():
+		if not c == '\n':
+			dwnlvl = (ranks(l, pref=pref+c))
+			if len(dwnlvl)>0:
+				items.extend(dwnlvl)
+		elif l > 0:
+			items.append((pref,l))
+	return items
+
+
+
+wdex = re.compile('^[A-Za-z][a-z]{2,20}$') #, re.I)
 voc = {}
 dep = 0
 for word in open('/usr/share/dict/american-english', 'r'):
@@ -37,16 +50,30 @@ found = []
 for i in range(len(s)-2):
 	lvl = voc
 	rec = ''
-	for c in s[i:i+15].lower():
-		if '\n' in lvl:
-			found.append(rec)
-			lvl['\n'] = lvl.get('\n', 0) + 1
+	for c in s[i:i+20].lower():
 		if c in lvl:
 			rec += c
 		else:
+			if '\n' in lvl:
+				found.append(rec)
+				lvl['\n'] = lvl.get('\n', 0) + 1
 			break
 		lvl = lvl.get(c)
 
-print(found)
+#print(found)
 
-show(voc)
+#show(voc)
+scores = ranks(voc)
+freq = 0
+line = []
+for t,f in sorted(scores, key=lambda t:t[1], reverse=True):
+	if f > 0:
+		if f != freq:
+			print('\n   '.join([' '.join(line[i:i+8]) for i in range(0,len(line), 8)]))
+			freq = f
+			line = ['{} times:'.format(f)]
+		line += ['{}'.format(t)]
+print('\n   '.join([' '.join(line[i:i+8]) for i in range(0,len(line), 8)]))
+#print scores
+#print voc
+
