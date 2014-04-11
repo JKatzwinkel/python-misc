70a41b30d2cba296fe7060d24ea484e8abdd3110
J.Katzwinkel <jkatzwinkel@github.com>
2014-04-11 02:12:43 +0200

    python 3 seems to turn sucking

--- s/sv.py
+++ s/sv.py
@@ -2,7 +2,19 @@
 
 import socket
 import sys
+import sqlite3 as sql
+from time import time
 
+
+dbcon = sql.connect('list.db')
+dbcsr = dbcon.cursor()
+items = {}
+
+def shoppinglist():
+	return u'\n'.join(['Einkaufen:']+[' {}'.format(i) for i in list(items.keys())]+[''])
+
+def splitline(line):
+	return {i.strip():time() for i in line.split(' ') if len(i)>0}
 
 
 
@@ -11,32 +23,32 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 try:
 	s.bind(('', 10000))
 except:
-	print 'Socket already in use.'
+	print('Socket already in use.')
 	s.close()
 	sys.exit(1)
 
-
 s.listen(0)
-connection, addr = s.accept()
-print addr
 
-items = []
 while 1:
+	connection, addr = s.accept()
+	print(addr)
+	data = b''
 	data = connection.recv(1024)
-	if not data:
-		break
-	print '[{0}]'.format(data)
-	if data.startswith('GET'):
-		#connection.sendall('\n'.join([line for line in open('index.html')]))
-		connection.sendall('OK.\n')
-		break
-	items.extend([i.strip() for i in data.split(' ') if len(i)>0])
+	while data:
+		data = data.decode('utf-8')
+		print(u'[{0}]'.format(data))
+		if data.startswith('GET'):
+			#connection.sendall('\n'.join([line for line in open('index.html')]))
+			connection.sendall(bytes(shoppinglist(), 'utf-8'))
+			break
+		items.update(splitline(data))
+		data = connection.recv(1024)
 # close connection
-connection.close()
+	connection.close()
 # if item list was received
-if len(items) > 0:
-	for i in items:
-		print i
+	if len(items) > 0:
+		for i in items:
+			print('[{}]'.format(i))
 # otherwise
 #else:
 #s.connect(addr)
