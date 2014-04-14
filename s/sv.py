474703ccef1ea956226831a19c430a086ae21d85
J.Katzwinkel <jkatzwinkel@github.com>
2014-04-14 14:50:25 +0200

    sl: persistent

--- s/sv.py
+++ s/sv.py
@@ -6,37 +6,53 @@ import sqlite3 as sql
 import time
 
 
-dbcon = sql.connect('list.db')
-dbcsr = dbcon.cursor()
-items = {}
+# connect to database
+db = sql.connect('list.db')
+dbc = db.cursor()
+# initialize if necessary
+dbc.execute('''create table if not exists needs (name text,
+time int)''')
 
+items = {}
+# read item list from db, if possible
+for row in dbc.execute('select * from needs'):
+	k,v = row
+	items[k] = float(v)
+
+
+# returns single line in formatted string
 def fmtitem(item):
 	lct = time.gmtime(item[1])
 	#return '  {} [{}]'.format(item[0],
 		#time.strftime('%Y-%m-%d', lct))
 	return '  {}'.format(item[0])
 
+# assemble text output for shopping list
 def shoppinglist():
 	return u'\n'.join(['Einkaufen:']+[fmtitem(i) for i in itemschron()]+[''])
 
+# split string into single items and put in dict along timestamps
 def splitline(line):
 	return {i.strip():time.time() for i in line.split(' ') if len(i)>0}
 
+# sort items dictionary chronologically
 def itemschron(asc=False):
 	return sorted(items.items(), key=lambda t:t[1], reverse=not asc)
 
+# open socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# bind port and let listen
 try:
 	s.bind(('', 10000))
 except:
 	print('Socket already in use.')
 	s.close()
 	sys.exit(1)
-
 s.listen(0)
 
 while 1:
+	# establish connection once request comes in
 	connection, addr = s.accept()
 	print(addr)
 	data = b''
@@ -48,22 +64,27 @@ while 1:
 			#connection.sendall('\n'.join([line for line in open('index.html')]))
 			connection.sendall(bytes(shoppinglist(), 'utf-8'))
 			break
-		items.update(splitline(data))
+		items_new = splitline(data)
+		# update items dict with newly read items
+		items.update(items_new)
+		# update data base
+		for k, v in items_new.items():
+			dbc.execute('''insert into needs values ('{}', {})'''.format(
+				k, int(v)))
+		# socket read on
 		data = connection.recv(1024)
-# close connection
+	# comit database queries
+	db.commit()
+	# close connection
 	connection.close()
-# if item list was received
-	if len(items) > 0:
-		for i in items:
-			print('[{}]'.format(i))
-# otherwise
-#else:
-#s.connect(addr)
-	#s.sendall('Liste:\nstuff')
+
+
+# shutdown socket
 s.shutdown(socket.SHUT_RDWR)
 s.close()
 
-
+# shutdown db conneection
+db.close()
 #s.connect(('', 10000))
 
 
